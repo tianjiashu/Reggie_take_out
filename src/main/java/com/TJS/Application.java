7b25ca2f59cf7d9b1954e98586f96aa173145ecb
LuckyDog <92830810@qq.com>
2022-07-31 14:56:48 +0800

    添加开启缓存注解功能

--- src/main/java/com/TJS/Application.java
+++ src/main/java/com/TJS/Application.java
@@ -4,6 +4,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.transaction.event.TransactionalEventListener;
 
@@ -11,6 +12,7 @@ import org.springframework.transaction.event.TransactionalEventListener;
 @SpringBootApplication
 @ServletComponentScan
 @EnableTransactionManagement
+@EnableCaching  //开启缓存注解功能
 /**
  *org.springframework.beans.factory.BeanCreationException: Error creating bean with name 'routerFunctionMapping' defined in class path resource [com/TJS/reggie/config/WebMvcConfig.class]: Bean instantiation via factory method failed; nested exception is org.springframework.beans.BeanInstantiationException: Failed to instantiate [org.springframework.web.servlet.function.support.RouterFunctionMapping]: Factory method 'routerFunctionMapping' threw exception; nested exception is java.lang.NoClassDefFoundError: com/fasterxml/jackson/dataformat/cbor/CBORFactory
  *
