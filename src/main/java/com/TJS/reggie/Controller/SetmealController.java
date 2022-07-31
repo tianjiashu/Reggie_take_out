7b31ae371720bb8f45fa5876dc07ca1ecfc4f59b
LuckyDog <92830810@qq.com>
2022-07-31 14:55:03 +0800

    使用SpringCache优化套餐查询

--- src/main/java/com/TJS/reggie/Controller/SetmealController.java
+++ src/main/java/com/TJS/reggie/Controller/SetmealController.java
@@ -10,6 +10,8 @@ import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
@@ -33,6 +35,7 @@ public class SetmealController {
 
 
     @PostMapping
+    @CacheEvict(value = "SetmealCache",allEntries = true )
     public R<String> save(@RequestBody SetmealDto dto){
         setmealService.saveWithmeal(dto);
         return R.success("存储成功");
@@ -62,11 +65,15 @@ public class SetmealController {
     }
 
     @DeleteMapping
+    @CacheEvict(value = "SetmealCache",allEntries = true )
     public R<String> delete(@RequestParam List<Long> ids){
         setmealService.deleteWithdish(ids);
         return R.success("删除成功！");
     }
+
+
     @PostMapping("/status/{status}")
+    @CacheEvict(value = "SetmealCache",allEntries = true )//删除SetmealCache中所有缓存
     public R<String> update(@RequestParam List<Long> ids,@PathVariable int status){
         List<Setmeal> setmeals = new ArrayList<>();
         for (Long id : ids) {
@@ -82,6 +89,7 @@ public class SetmealController {
     }
 
     @GetMapping("list")
+    @Cacheable(value = "SetmealCache",key = "#setmeal.categoryId",unless = "#result.data==null")
     public R<List<Setmeal>> list(Setmeal setmeal){
         log.info("{}",setmeal.toString());
         LambdaQueryWrapper<Setmeal> queryWrapper = new LambdaQueryWrapper<>();
@@ -96,4 +104,8 @@ public class SetmealController {
 
         return R.success(setmeals);
     }
+    /**
+     * http://localhost/setmeal/1415580119015145474
+     * 修改套餐  Get
+     */
 }
